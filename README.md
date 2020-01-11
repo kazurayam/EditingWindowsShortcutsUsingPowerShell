11209dd6d1c0113b38cc57723f71004c75d79e41
kazurayam <kazuaki.urayama@gmail.com>
2020-01-11 22:52:11 +0900

    updates

---
+++ README.md
@@ -0,0 +1,7 @@
+PowerShellで絶対パスと相対パスを相互変換する
+==============
+
+
+```
+New-Fixture -Path $env:USERPROFILE\Documents\WindowsPowerShell\Modules\FizzBuzz -Name FizzBuzz
+```
---
+++ TryResolvePath.psm1
@@ -0,0 +1,26 @@
+﻿filter Convert-LocalPath {
+    param (
+        [parameter(
+            Mandatory=$true,
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [string] $Path,
+
+        [ValidateNotNullOrEmpty()]
+        [uri] $Location = (Get-Location).Path,
+
+        [switch] $Relative
+    )
+
+    if (-not $Relative) {
+        return [uri]::new($Location, $Path).LocalPath
+    }
+
+    $localPath = $Location.MakeRelative($Path) -replace '/', '\'
+    if ($localPath.StartsWith('.')) {
+        $localPath
+    } else {
+        ".\${localPath}"
+    }
+}
