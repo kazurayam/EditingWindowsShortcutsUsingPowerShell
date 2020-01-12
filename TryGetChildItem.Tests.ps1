243b232e4831e12fc4bd825d476f79c1616d12b5
kazurayam <kazuaki.urayama@gmail.com>
2020-01-12 12:10:24 +0900

    update TryGetChildItem.Tests.ps1

--- TryGetChildItem.Tests.ps1
+++ TryGetChildItem.Tests.ps1
@@ -9,6 +9,9 @@ Describe "TryGetChildItem" {
     }
 
     It "ショートカットのリンク先を参照してコンソールにprintする"　{
-        
+        $currentDir = Get-Location
+        $WScript = New-Object -ComObject WScript.Shell
+        Get-ChildItem -Path $currentDir\fixture\* -Recurse -Include *.lnk | ForEach-Object {$WScript.CreateShortcut($_.FullName).TargetPath} | Out-File -FilePath $currentDir\output\targetPaths.txt
+
     }
 }
