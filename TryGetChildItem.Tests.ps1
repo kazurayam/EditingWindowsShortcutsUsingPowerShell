6a1cb33bcc71be25903070298a072f9a6e7bf3ef
kazurayam <kazuaki.urayama@gmail.com>
2020-01-12 11:58:39 +0900

    add TryGetChildItem.Tests.ps1 and the test fixture

---
+++ TryGetChildItem.Tests.ps1
@@ -0,0 +1,14 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "TryGetChildItem" {
+    It "カレントディレクトリ/fixtureフォルダの下に*.lnkファイルが2つ存在する" {
+        $currentDir = Get-Location
+        (Get-ChildItem -Path $currentDir\fixture\* -Recurse -Include *.lnk | Measure-Object).Count | Should Be 2
+    }
+
+    It "ショートカットのリンク先を参照してコンソールにprintする"　{
+        
+    }
+}
