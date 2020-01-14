55a5cea9d7baca111a4bdda8b90b81100c91cba8
kazurayam <kazuaki.matsuhashi@quick.jp>
2020-01-14 11:34:03 +0900

    introlduced try {} finally {}

--- TryResolvePath.Tests.ps1
+++ TryResolvePath.Tests.ps1
@@ -2,24 +2,26 @@
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.ps1', '.psm1'
 Import-Module "$here\$sut" -Force
 
-Describe "TestResolvePath" {
+Describe "コマンドレット Resolve-Path を試す" {
     <#
-    It "does something useful" {
-        $true | Should Be $false
-    }
+    https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/resolve-path?view=powershell-7
     #>
     It "絶対パスから相対パスへの変換" {
-        $base = "C:\Users\qqf73\github"    # 基準となるパス
-        $abs  = "C:\Users\qqf73\github\ResolvePathInPowerShell\README.md"    # 絶対パス
+        $base = "${env:USERPROFILE}\github"    # 基準となるパス
+        $abs  = "${env:USERPROFILE}\github\EditingWindowsShortcutsUsingPowerShell\README.md"    # 絶対パス
         Push-Location $base
-            Resolve-Path $abs -Relative | Should Be '.\ResolvePathInPowerShell\README.md'
+        try {
+            Resolve-Path $abs -Relative | Should Be '.\EditingWindowsShortcutsUsingPowerShell\README.md'
             $base | Test-Path
             $abs  | Test-Path
-        Pop-Location
-        # Push-LocationとPop-Locationは関数内でカレントディレクトリを
-        # 変更するような処理をlocation stack上で行う。
-        # カレントディレクトリが本当に変更すれば呼び出し元の処理に影響が及ぶが
-        # スタックを利用することで影響を与えないようにする
+        } finally {
+            Pop-Location
+        }
+        <#
+        Push-LocationとPop-Locationは関数内でカレントディレクトリを変更するような処理をlocation stack上で行う。
+        カレントディレクトリが本当に変更すれば呼び出し元の処理に影響が及ぶのでややこしいことになるが、
+        Push-LocationとPop-Locationを使うことによりカレントディレクトリに影響を与えずにパスの変換処理を実行することができる。
+        #>
     }
     It "相対パスから絶対パスへの変換例" {
         $base = "X:\foo\bar\"
