2531085a15a8f8a3d279fbbe84270091407197a3
kazurayam <kazuaki.matsuhashi@quick.jp>
2020-01-17 13:10:22 +0900

    ShortcutOperations.Tests.ps1 works complete

--- src/test/powershell/ShortcutOperations.Tests.ps1
+++ src/test/powershell/ShortcutOperations.Tests.ps1
@@ -5,33 +5,68 @@
 Describe "Windowsショートカットにたいして様々の操作を試す" {
     
     $currentDir = Get-Location
-    $workdir = Join-Path $currentDir "build\tmp\testOutput\ShortcutOperations"
+    $workDir = Join-Path $currentDir "build\tmp\testOutput\ShortcutOperations"
+    $wsh = New-Object -ComObject WScript.shell 
 
-    It "既にあるショートカットのリンク先を読み出して表示する" {
-        Write-Host "Hello"
-        $path = Join-Path $workdir "subfolder\moreLinkToAppData.lnk"
-        $wsh = New-Object -ComObject WScript.shell 
+    It "既にあるショートカットのリンク先を読み出す" {
+        $path = Join-Path $workDir "subfolder\moreLinkToAppData.lnk"
         $shortcut = $wsh.createShortcut($path)
-        $targetPath = $shortcut.targetPath    # C:\Users\qcq0264\AppData
-        $targetPath | Should Match "^C:\\Users"
-        $targetPath | Should Match "\\AppData$"
-
+        $target = $shortcut.targetPath
+        # このショートカットのリンク先は C:\Users\qcq0264\AppData であるはず
+        # Write-Host "targetPath is ${targetPath}"
+        $target | Should Match "^C:\\Users"
+        $target | Should Match "\\AppData$"
     }
 
-    It "あるフォルダの下にあるすべてのショートカットのリンク先を読み出して表示する" {
+    It "wordirフォルダの下にあるすべてのショートカットのリンク先を読み出す" {
+        Get-ChildItem $workDir -Recurse -Filter *.lnk | ForEach-Object {
+            $path = $_.FullName
+            $shortcut = $wsh.createShortcut($path)
+            $target = $shortcut.targetPath
+            # Write-Host "${path} has targetPath=${targetPath}"
+            $target | Should Match "\\AppData$"
+        }
     }
 
-    It "既にあるショートカットのリンク先を変更して保存する" {
+    It "既にあるショートカットのリンク先を C:\Users\xxxx\AppData から C:\Users\xxxx\AppData\Roaming\Google\Chrome に変更して保存する" {
+        $path = Join-Path $workDir "subfolder\moreLinkToAppData.lnk"
+        $shortcut = $wsh.createShortcut($path)
+        if ($shortcut.targetPath -Match 'AppData$') {
+            # 新しく設定するリンク先文字列を求めて
+            $targetPath = $shortcut.targetPath -replace 'AppData$', 'AppData\Roaming\Google\Chrome'
+            # ショートカットを変更する
+            $new = $wsh.createShortcut($path)
+            $new.targetPath = $targetPath
+            $new.save()
+            # ショートカットのtargetPathが期待した値に変更されているかどうか確かめる
+            $sht = $wsh.createShortcut($path)
+            $sht.targetPath | Should Match 'Chrome$'
+        }
     }
 
     It "あるフォルダの下にあるすべてのショートカットのリンク先を変更して保存する" {
+        Get-ChildItem $workDir -Recurse -Filter *.lnk | ForEach-Object {
+            $path = $_.FullName
+            $shortcut = $wsh.createShortcut($path)
+            if ($shortcut.targetPath -Match 'AppData$') {
+                # 新しく設定するリンク先文字列を求めて
+                $targetPath = $shortcut.targetPath -replace 'AppData$', 'AppData\Roaming\Google\Chrome'
+                # ショートカットを変更する
+                $new = $wsh.createShortcut($path)
+                $new.targetPath = $targetPath
+                $new.save()
+                # ショートカットのtargetPathが期待した値に変更されているかどうか確かめる
+                $sht = $wsh.createShortcut($path)
+                $sht.targetPath | Should Match 'Chrome$'
+            }
+        }
     }
 
     BeforeEach {
         # srcの下にあるfixtureをbuildにコピーする
         # 各テストがfixtureを変更しても他テストに影響を及ぼさないようにするため
         $source = Join-Path $currentDir "src/test/fixture"
-        $destination = $workdir
+        $destination = $workDir
         # destionationディレクトリを削除し再作成する
         Remove-Item $destination -Force -Recurse
         New-Item $destination -ItemType Directory
