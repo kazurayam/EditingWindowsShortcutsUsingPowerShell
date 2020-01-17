e08541f77da9d9759c008b088f12fceff987d0de
kazurayam <kazuaki.matsuhashi@quick.jp>
2020-01-17 10:21:23 +0900

    Pester tests must be named *.Tests.ps1

---
+++ src/test/powershell/ShortcutOperations.Test.ps1
@@ -0,0 +1,25 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+# $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+# . "$here\$sut"
+
+Describe "Windowsショートカットにたいして様々の操作を試す" {
+    It "既にあるショートカットのリンク先を読み出して表示する" {
+
+    }
+
+    It "既にあるショートカットのリンク先を変更して保存する" {
+
+    }
+
+    It "あるフォルダの下にあるすべてのショートカットのリンク先を読み出して表示する" {
+
+    }
+
+    It "あるフォルダの下にあるすべてのショートカットのリンク先を変更して保存する" {
+
+    }
+
+    BeforeEach {
+
+    }
+}
